efe3e8b4a4c6dd700751514efb91c65035eb6218
Jay <jaywhang83@gmail.com>
2016-05-03 14:32:36 -0700

    Add test for location model

---
+++ TravelingNerds.Tests/LocationTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Traveling_Nerds.Models;
+using Xunit;
+
+namespace TravelingNerds.Tests
+{
+    public class LocationTest
+    {
+        [Fact]
+        public void GetNameTest()
+        {
+            // Arrange
+            var location = new Location();
+            location.Name = "Croatia"; 
+
+            // Act
+            var result = location.Name;
+
+            // Assert
+            Assert.Equal("Croatia", result); 
+        }
+    }
+}
