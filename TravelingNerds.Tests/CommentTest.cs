44066297da8abb8b43027e97fa4ad0e6b89e95cc
Jay <jaywhang83@gmail.com>
2016-05-03 14:45:15 -0700

    Add testfor comment model

---
+++ TravelingNerds.Tests/CommentTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Traveling_Nerds.Models;
+using Xunit;
+
+namespace TravelingNerds.Tests
+{
+    public class CommentTest
+    {
+        [Fact]
+        public void GetAuthorTest()
+        {
+            // Arrange
+            var comment = new Comment();
+            comment.Author = "Mike Jones";
+
+            // Act
+            var result = comment.Author;
+
+            // Assert
+            Assert.Equal("Mike Jones", result);
+        }
+
+        [Fact]
+        public void GetContentTest()
+        {
+            // Arrange
+            var comment = new Comment();
+            comment.Content = "Croatia looks amazing!";
+
+            // Act
+            var result = comment.Content;
+
+            // Assert
+            Assert.Equal("Croatia looks amazing!", result);
+        }     
+    }
+}
